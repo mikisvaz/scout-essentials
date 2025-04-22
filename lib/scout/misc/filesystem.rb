a5b2cdd7012f442e782c73f4c70521188e719048
Miguel Vazquez <mikisvaz@gmail.com>
2025-04-22 16:27:07 +0200

    Ruby only version of tarize

--- lib/scout/misc/filesystem.rb
+++ lib/scout/misc/filesystem.rb
@@ -23,13 +23,44 @@ module Misc
     end
   end
 
-  def self.tarize(path, dest = nil)
+  def self.tarize_cmd(path, dest = nil)
     Misc.in_dir(path) do
       if dest
         CMD.cmd("tar cvfz '#{dest}' '.'")
       else
         CMD.cmd("tar cvfz - '.'", :pipe => true)
       end
+    end
+  end
+
+  def self.tarize(source_dir, archive_path)
+    require 'rubygems/package'
+    require 'zlib'
+    require 'stringio'
+    require 'fileutils'
+
+    tar_io = StringIO.new("")
+
+    Gem::Package::TarWriter.new(tar_io) do |tar|
+      Dir[File.join(source_dir, '**', '*')].each do |file|
+        relative_path = file.sub(/^#{Regexp.escape(source_dir)}\/?/, '')
+
+        if File.directory?(file)
+          tar.mkdir(relative_path, File.stat(file).mode)
+        else
+          tar.add_file(relative_path, File.stat(file).mode) do |tf|
+            File.open(file, 'rb') { |f| tf.write(f.read) }
+          end
+        end
+      end
+    end
+
+    tar_io.rewind
+
+    File.open(archive_path, 'wb') do |f|
+      gz = Zlib::GzipWriter.new(f)
+      gz.write(tar_io.string)
+      gz.close
     end
   end
 
