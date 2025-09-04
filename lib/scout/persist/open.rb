4899d689a473ead0d41f1b5d1336fd79a4311d56
Miguel Vazquez <mikisvaz@gmail.com>
2025-09-04 17:54:24 +0200

    Open json and yaml with extension

--- lib/scout/persist/open.rb
+++ lib/scout/persist/open.rb
@@ -4,10 +4,12 @@ require 'yaml'
 
 module Open
   def self.json(file)
+    file = file.find_with_extension :json if Path === file
     Open.open(file){|f| JSON.load(f) }
   end
 
   def self.yaml(file)
+    file = file.find_with_extension :yaml if Path === file
     Open.open(file){|f| YAML.unsafe_load(f) }
   end
 
